8f8a38db87e820fc8f3a9bcb30501dcb435a9076
Sathvik <dachepallisathvik@gmail.com>
2023-03-15 17:37:26 +0530

    15/3 commit

--- Exercise/urls.py
+++ Exercise/urls.py
@@ -3,16 +3,10 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from Exercise.views import *
-from rest_framework.routers import DefaultRouter
 
-# router=DefaultRouter()
-
-# router.register(r'Exercise',ExerciseView)
-urlpatterns=[
-    # path("api/",include(router.urls)),
-    # path("", TestViewAPI.as_view(),name="Test")
-    path("",ExerciseView.as_view()),
-    path("crud/<int:pk>",CRUDExerciseView.as_view())
+urlpatterns=[ 
+    path("api",ExerciseView.as_view()),
+    path("api/crud/<int:pk>",CRUDExerciseView.as_view())
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ExerciseAPI/urls.py
+++ ExerciseAPI/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path,include
-from rest_framework.authtoken.views import obtain_auth_token
 from Exercise.views import ExerciseView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -14,7 +13,6 @@ admin.site.index_title="Welcome Admin"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include('Exercise.urls')),
-    path("apitoken",obtain_auth_token),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

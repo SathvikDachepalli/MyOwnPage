b8a09c2c75a7ea090c685a1bbfc409c4c194aaa8
Sathvik <dachepallisathvik@gmail.com>
2023-03-15 17:51:13 +0530

    MyPage app created

--- ExerciseAPI/urls.py
+++ ExerciseAPI/urls.py
@@ -4,9 +4,6 @@ from Exercise.views import ExerciseView
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-
-
 admin.site.site_header="Exercise API"
 admin.site.site_title="Exercise page"
 admin.site.index_title="Welcome Admin"
